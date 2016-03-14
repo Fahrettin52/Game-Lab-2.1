25fef334b80d1191eda47fd57eab5b78a7377469
M. Mendes Moreira <kcamvankester@hotmail.com>
2016-03-14 11:42:55 +0100

    shizzle voor sprint 2
    heb packmanager af en moet nog wat tweaken bij Death(); in de Health
    script.heb Rebirth met Death samengevoegd en dus Rebirth verwijdert uit
    ons UML

---
+++ Development/Game-Lab-2.1/Colony Cruiser The Maria/Assets/_Scripts/Items/PackManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PackManager : MonoBehaviour {
+	public enum PackType{
+		MedPack,
+		StimPack
+	}
+	public PackType curPackType;
+
+	public int medPackCount;
+	public int stimPackCount;
+
+	void Update () {
+		if(Input.GetButtonDown("SwitchPack")){
+			SwitchPack ();
+		}
+		switch (curPackType) {
+		case PackType.MedPack:
+			MedPackChecker ();
+			break;
+		case PackType.StimPack:
+			StimPackChecker ();
+			break;
+		}
+	}
+
+	public void MedPackChecker(){
+		if(medPackCount > 0 && Input.GetButtonDown("ActivatePack")){
+			print ("heal de speler");
+			medPackCount--;
+		}
+		else if (medPackCount < 1 && stimPackCount > 0) {
+			SwitchPack ();
+		}
+	}
+
+	public void StimPackChecker(){
+		if (stimPackCount > 0 && Input.GetButtonDown ("ActivatePack")) {
+			print ("Activer slow-mo");
+			stimPackCount--;
+		} 
+		else if (stimPackCount < 1 && medPackCount > 0) {
+			SwitchPack ();
+		}
+	}
+
+	public void SwitchPack(){
+		switch (curPackType) {
+		case PackType.MedPack:
+			curPackType = PackType.StimPack;
+			break;
+		case PackType.StimPack:
+			curPackType = PackType.MedPack;
+			break;
+		}
+	}
+}
---
+++ Development/Game-Lab-2.1/Colony Cruiser The Maria/Assets/_Scripts/Player/Health.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+	public int health;
+	public int maxHealth;
+	private int startHealth;
+	public Transform respawnPoint;
+
+	void Start () {
+		startHealth = health;
+		maxHealth = health;
+	}
+
+	//Dit is om healing en damaging te testen
+//	void Update () {
+//		if(Input.GetButtonDown("Jump")){
+//			Damaging (25);
+//		}
+//		if(Input.GetButtonDown("ActivatePack")){
+//			Healing (25);
+//		}
+//	}
+
+	public void Healing(int toHeal){
+		if(health < maxHealth){
+			health += toHeal;
+			if(health > maxHealth){
+				health = maxHealth;
+			}
+		}
+	}
+
+	public void Damaging(int toDamage){
+		if (health > 1) {
+			health -= toDamage;
+			if (health < 1) {
+				health = 0;
+				Death ();
+			}
+		}
+	}
+
+	public void Death(){
+		print ("Dieded!!");
+		//Speel hier de death/gameover screen af
+		transform.position = respawnPoint.position;
+		transform.rotation = respawnPoint.rotation;
+		health = startHealth;
+	}
+}
