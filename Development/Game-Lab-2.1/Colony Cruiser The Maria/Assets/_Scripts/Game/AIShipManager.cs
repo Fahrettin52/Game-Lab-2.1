313e8328b7896eaef27f9545cf6106a1473fffd1
Fahrettin52 <Fahrettin5252@hotmail.com>
2016-03-22 12:42:56 +0100

    AI Conversation af & Asset list update artist en development

---
+++ Development/Game-Lab-2.1/Colony Cruiser The Maria/Assets/_Scripts/Game/AIShipManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AIShipManager : MonoBehaviour {
+
+    public string[] currentInfo;
+    public Text AI;
+
+    public void Start() {
+        DialogeChecker(0);
+    }
+
+    public void DialogeChecker(int counter) {
+        AI.text = currentInfo[counter];
+    }
+}
---
+++ Development/Game-Lab-2.1/Colony Cruiser The Maria/Assets/_Scripts/Game/ConversationTrigger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConversationTrigger : MonoBehaviour {
+
+    public GameObject AI;
+    public int arrayCurrent;
+
+    public void Start() {
+        AI = GameObject.Find("AIShip");
+    }
+
+    public void OnCollisionEnter(Collision collision) {
+        if (collision.transform.tag == "Player") {
+            arrayCurrent++;
+            AI.GetComponent<AIShipManager>().DialogeChecker(arrayCurrent);
+            Destroy(gameObject);
+        }
+    }
+}
