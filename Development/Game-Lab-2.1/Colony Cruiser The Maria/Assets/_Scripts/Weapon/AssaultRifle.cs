9559caf71c8dec90d898aec91ea0daaaabd01ca7
Fahrettin52 <Fahrettin5252@hotmail.com>
2016-03-24 13:28:52 +0100

    Uren registratie & begin Script Assaultrifle

---
+++ Development/Game-Lab-2.1/Colony Cruiser The Maria/Assets/_Scripts/Weapon/AssaultRifle.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AssaultRifle : AbstractWeapon {
+
+	public override void Shooting(){}
+
+	public override void Reloading(){}
+
+	public override void OnEnable (){}
+
+	public override bool Aiming(bool aim){
+		return aim;
+	}
+
+	public override void AmmoRemove(){}
+
+	public override void AmmoAdd(){}
+
+	public override void AmmoEffect(){}
+
+	public override void HitChecker(){}
+
+	public override void FillDelegate (){}
+
+	public override void DistanceChecker (Vector3 savedPos){}
+
+	public override IEnumerator ImpactDelay (float impactTime, float damage){
+		yield return new WaitForSeconds(impactTime);
+	}
+
+	public override void QuickMelee (){}
+
+	public override void UIChecker(){}
+}
