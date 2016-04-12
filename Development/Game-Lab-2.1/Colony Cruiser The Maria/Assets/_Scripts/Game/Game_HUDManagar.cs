63a42722fc51e0869947e69e320f2ef2bfb408dd
Fahrettin52 <Fahrettin5252@hotmail.com>
2016-04-12 10:46:31 +0200

    Assetlist Update & Start HUD Manager

---
+++ Development/Game-Lab-2.1/Colony Cruiser The Maria/Assets/_Scripts/Game/Game_HUDManagar.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game_HUDManagar : MonoBehaviour {
+
+	public void StateChecker(){
+	}	
+
+	public void StartButton(){
+	}
+
+	public void LoadButton(){
+	}
+
+	public void OptionButton(){
+	}
+
+	public void QuitButton(){
+	}
+
+	public void CreditButton(){
+	}
+}
