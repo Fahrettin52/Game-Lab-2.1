0624a3bef79d4d58a6a01786fd2ccad4405018af
Fahrettin52 <Fahrettin5252@hotmail.com>
2016-04-12 17:11:25 +0200

    Uren Registratie 12-04-2016 & Asset List Update & Menu

--- Development/Game-Lab-2.1/Colony Cruiser The Maria/Assets/_Scripts/Game/GameHUDManagar.cs
+++ Development/Game-Lab-2.1/Colony Cruiser The Maria/Assets/_Scripts/Game/GameHUDManagar.cs
@@ -1,13 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEditor.SceneManagement;
 
 public class GameHUDManagar : MonoBehaviour {
 
+	public enum MenuType { start, load, option, credit }
+
+	public MenuType myMenuType;
+
 	public void StateChecker(){
+		switch(myMenuType){
+		case MenuType.start:
+			StartButton();
+			break;
+
+		case MenuType.load:
+			LoadButton();
+			break;
+
+		case MenuType.option:
+			OptionButton();
+			break;
+
+		case MenuType.credit:
+			CreditButton();
+			break;
+		}
 	}	
 
 	public void StartButton(){
-		Application.LoadLevel ("Verdieping 3"); 
+		EditorSceneManager.LoadScene ("Verdieping 3"); 
 	}
 
 	public void LoadButton(){
