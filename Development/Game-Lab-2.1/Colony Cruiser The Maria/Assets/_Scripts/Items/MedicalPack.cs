e38640e128789501e21cd035b964c0376e21bc5c
Fahrettin52 <Fahrettin5252@hotmail.com>
2016-04-15 07:48:16 +0200

    UML & Asset list update

---
+++ Development/Game-Lab-2.1/Colony Cruiser The Maria/Assets/_Scripts/Items/MedicalPack.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthPack : AbstractPack {
+
+//	public void OnCollisionEnter(Collision col){
+//		AddPack ();
+//	}
+
+	public override void AddPack(){
+		player.GetComponent<PackManager> ().medPackCount++;
+	}
+}
