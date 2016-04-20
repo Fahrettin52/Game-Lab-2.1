329ba36948c9a7af1600c2e7ba612f69a225af88
Fahrettin52 <Fahrettin5252@hotmail.com>
2016-04-20 11:01:39 +0200

    AIEnemy Abstract af, Asset list en uren registratie

---
+++ Development/Game-Lab-2.1/Colony Cruiser The Maria/Assets/_Scripts/Enemy/AIEnemy.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class AIEnemy : AbstractEnemy {
+
+	public override void PlayerDetection(){
+	}
+
+	public override void HealthChecker(){
+	}
+
+	public override void AttackPlayer(){
+	}
+
+	public override void StateSwitcher(){		
+	}
+
+	public void LineOfSightDetection(){
+	}
+}
