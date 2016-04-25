b2687865b142dce42d21987c1998bfb81f5c0338
Fahrettin52 <Fahrettin5252@hotmail.com>
2016-04-25 09:21:31 +0200

    Menu return button af voor demo

---
+++ Development/Game-Lab-2.1/Colony Cruiser The Maria/Assets/_Scripts/Game/HUDManagar.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor.SceneManagement;
+
+public class HUDManagar : MonoBehaviour {
+
+	public GameObject mainMenu;
+	public GameObject load;
+	public GameObject option;
+	public GameObject credit;
+
+	public void StartButton(){
+		EditorSceneManager.LoadScene ("Verdieping 3"); 
+	}
+
+	public void LoadButton(){
+		mainMenu.SetActive (false);
+		load.SetActive (true);
+	}
+
+	public void OptionButton(){
+		mainMenu.SetActive (false);
+		option.SetActive (true);
+	}
+
+	public void QuitButton(){
+		mainMenu.SetActive (false);
+		Application.Quit();
+	}
+
+	public void CreditButton(){
+		mainMenu.SetActive (false);
+		credit.SetActive (true);
+	}
+
+	public void ReturnButton(){
+		mainMenu.SetActive (true);
+		load.SetActive (false);
+		option.SetActive (false);
+		credit.SetActive (false);
+	}
+}
