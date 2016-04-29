2d6b5819ae752ca92103ff8b2b65467cf0834efe
Fahrettin52 <Fahrettin5252@hotmail.com>
2016-04-29 13:46:14 +0200

    Uren Registratie & Convo survivior

---
+++ Development/Game-Lab-2.1/Colony Cruiser The Maria/Assets/_Scripts/Game/SurvivorManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SurvivorManager : MonoBehaviour {
+
+	public string[] survivorConversation;
+	public Text survivorText;
+
+	public void DialogeChecker(int counter){
+		survivorText.text = survivorConversation[counter];
+	}
+
+	public void DestroySurvivor(){
+		
+	}
+
+	public void OnTriggerEnter (Collider col){
+		if (col.transform.tag == "Player"){
+			DialogeChecker (0);
+		}
+	}
+}
