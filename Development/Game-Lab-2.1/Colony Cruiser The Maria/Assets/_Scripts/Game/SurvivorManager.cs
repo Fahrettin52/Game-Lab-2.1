abfc6cabaed704db795b7d6082f824894d9e4c0a
Fahrettin52 <Fahrettin5252@hotmail.com>
2016-05-11 10:59:18 +0200

    Survivor met continue button & UML Update & Asset List Update

--- Development/Game-Lab-2.1/Colony Cruiser The Maria/Assets/_Scripts/Game/SurvivorManager.cs
+++ Development/Game-Lab-2.1/Colony Cruiser The Maria/Assets/_Scripts/Game/SurvivorManager.cs
@@ -6,18 +6,32 @@ public class SurvivorManager : MonoBehaviour {
 
 	public string[] survivorConversation;
 	public Text survivorText;
+	public GameObject panel;
+	public int currentConvo;
 
 	public void DialogeChecker(int counter){
 		survivorText.text = survivorConversation[counter];
 	}
 
 	public void DestroySurvivor(){
-		
+		panel.SetActive (false);
+		Destroy (transform.gameObject);
 	}
 
 	public void OnTriggerEnter (Collider col){
-		if (col.transform.tag == "Player"){
-			DialogeChecker (0);
+		if(col.transform.tag == "Player"){
+			panel.SetActive (true);
+			DialogeChecker (currentConvo);
+		}
+	}
+
+	public void Continue(){	
+		currentConvo++;
+		if(currentConvo < survivorConversation.Length){
+			DialogeChecker (currentConvo);
+		}
+		if(currentConvo >= survivorConversation.Length){
+			DestroySurvivor ();
 		}
 	}
 }
