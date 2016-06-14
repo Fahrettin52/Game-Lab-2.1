7a547fbef42aca3c9c13bc503d040126a8b1c149
Fahrettin52 <Fahrettin5252@hotmail.com>
2016-06-14 19:38:41 +0200

    Lights flikkeren

---
+++ Development/Game-Lab-2.1/Colony Cruiser The Maria/Assets/_Scripts/Environment/LightManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightManager : MonoBehaviour {
+
+	public GameObject[] lightsHallway;
+	private float nextShine;
+	public float shiningRate;
+	public float maxRandomRange;
+
+	void FixedUpdate () {
+		if (Time.time > nextShine) {
+			for (int i = 0; i < lightsHallway.Length; i++) {
+				i = Random.Range (0, lightsHallway.Length);
+				StartCoroutine (lightSwitcher (i));
+				float randomRate = Random.Range (0, maxRandomRange);
+				nextShine = Time.time + shiningRate + randomRate;
+			}
+		}
+	}
+
+	IEnumerator lightSwitcher(int i){
+		lightsHallway [i].SetActive (false);
+		float randomNumber = Random.Range (0.2f, 1f);
+		yield return new WaitForSeconds (randomNumber);
+		lightsHallway [i].SetActive (true);
+	}
+}
