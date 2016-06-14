0f73a7b73fb2a03a9ed03761fe7d8c897078087e
Fahrettin52 <Fahrettin5252@hotmail.com>
2016-06-14 19:07:13 +0200

    Standard assets import en belichting

---
+++ Development/Game-Lab-2.1/Colony Cruiser The Maria/Assets/_Scripts/Environment/Lights.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lights : MonoBehaviour {
+
+	public GameObject light;
+	public float shiningRate;
+	private float nextShine;
+	private bool mayShine;
+	public float maxRandomRange;
+
+	void FixedUpdate(){
+		if (Time.time > nextShine) {
+			mayShine = !mayShine;
+			light.SetActive (mayShine);
+			float randomRate = Random.Range (0, maxRandomRange);
+			nextShine = Time.time + shiningRate + randomRate;
+		}
+	}
+}
